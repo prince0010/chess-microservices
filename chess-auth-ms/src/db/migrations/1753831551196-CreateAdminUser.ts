a5f980346cfd0062f0d1ca76a19e756c04e8196f
onier9702 <onier0217@gmail.com>
2025-08-01 08:28:55 -0600

    [bugfix] comma aggregated to create admin default migration file

--- chess-auth-ms/src/db/migrations/1753831551196-CreateAdminUser.ts
+++ chess-auth-ms/src/db/migrations/1753831551196-CreateAdminUser.ts
@@ -5,13 +5,13 @@ export class CreateAdminUser1753831551196 implements MigrationInterface {
     // insert super_admin user by default
     await queryRunner.query(
       `INSERT INTO auth (name, username, password, gender, country, roles)
-        VALUES ("Reino Digital", "admin@reinodigitalcr.com", "$2b$10$VRMO5.NxySnPdN6QhFWTlOWXvy.mcCxZA5NsuJvKT6RMK6MX.VE76", "MALE", "Cuba" "SUPER_ADMIN,ADMIN");`,
+        VALUES ("Reino Digital", "admin@reinodigitalcr.com", "$2b$10$VRMO5.NxySnPdN6QhFWTlOWXvy.mcCxZA5NsuJvKT6RMK6MX.VE76", "MALE", "Cuba", "SUPER_ADMIN,ADMIN");`,
     );
 
     // insert admin user by default
     await queryRunner.query(
       `INSERT INTO auth (name, username, password, gender, country, roles)
-        VALUES ("Arkadij Naiditsch", "Arkadij", "$2b$10$LBSLTL3PmpscV3TeffhjL.fOMEC2GKOtDZAW2mh8rfJZ6H7LXWHia", "MALE", "Russia" "ADMIN");`,
+        VALUES ("Arkadij Naiditsch", "Arkadij", "$2b$10$LBSLTL3PmpscV3TeffhjL.fOMEC2GKOtDZAW2mh8rfJZ6H7LXWHia", "MALE", "Russia", "ADMIN");`,
     );
   }
 
