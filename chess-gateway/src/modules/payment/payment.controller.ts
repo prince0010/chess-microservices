9bf029a26025c650a68e7bfdc44f309a4944b845
onier9702 <onier0217@gmail.com>
2025-11-07 14:56:03 -0600

    [update] removed gateway endpoint to forward stripe events and only handled stripe events from the same payment microservice directly

--- chess-gateway/src/modules/payment/payment.controller.ts
+++ chess-gateway/src/modules/payment/payment.controller.ts
@@ -8,18 +8,5 @@ import { NATS_SERVICE } from 'src/config';
 export class PaymentController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
-  // Production/Test — forward webhook to microservice
-  @Post('webhook')
-  async sendStripeWebhookToMicroservice(
-    @Req() req: Request,
-    @Res() res: Response,
-  ) {
-    // Send raw body + headers to microservice for signature verification
-    this.client.emit('payment.listen.webhookStripe', {
-      headers: req.headers,
-      rawBody: req['rawBody'],
-    });
-
-    return res.status(200).send('Webhook sent to payment microservice');
-  }
+  // not used at the moment
 }
--- chess-payment-ms/src/modules/payment/payment.controller.ts
+++ chess-payment-ms/src/modules/payment/payment.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Req, Res } from '@nestjs/common';
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Request, Response } from 'express';
 
 import { PaymentService } from './payment.service';
@@ -16,32 +16,9 @@ export class PaymentController {
     return this.paymentService.createPaymentSession(paymentSessionDto);
   }
 
-  // stripe events for localhost development with CLI
+  // stripe events
   @Post('webhook')
   async stripeWebhook(@Req() req: Request, @Res() res: Response) {
     return this.paymentService.stripeWebhook(req, res);
-  }
-
-  // Stripe events for production/test forwarded by Gateway via NATS
-  @EventPattern('payment.listen.webhookStripe')
-  async stripeWebhookViaNATS(@Payload() payload: any) {
-    console.log('HEREEEEE');
-    // Reconstruct mock request & response objects for compatibility
-    const mockReq: Partial<any> = {
-      headers: payload.headers,
-      rawBody: payload.rawBody,
-    };
-
-    const mockRes = {
-      status: () => ({
-        send: () => {},
-        json: () => {},
-      }),
-    } as unknown as Partial<Response>;
-
-    return await this.paymentService.stripeWebhook(
-      mockReq as Request,
-      mockRes as Response,
-    );
   }
 }
