f22ce102ccc72f3c613b6236f41d2397e9668aaa
onier9702 <onier0217@gmail.com>
2025-12-01 15:33:58 -0600

    [update] CRUD of coaches from auth-ms developed and endpoint create- findOne, findAll, update and remove softDelete added Part II

--- chess-auth-ms/src/modules/coaches/coaches.controller.ts
+++ chess-auth-ms/src/modules/coaches/coaches.controller.ts
@@ -1,35 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 import { CoachesService } from './coaches.service';
 
 import { CreateCoachDto, UpdateCoachDto } from './dto/create-coach.dto';
+import { FindAllCoachesDto } from './dto/find-all-coaches.dto';
 
 @Controller()
 export class CoachesController {
   constructor(private readonly coachesService: CoachesService) {}
 
-  @MessagePattern('createCoach')
+  @MessagePattern('coach.create.one')
   create(@Payload() createCoachDto: CreateCoachDto) {
     return this.coachesService.create(createCoachDto);
   }
 
-  @MessagePattern('findAllCoaches')
-  findAll() {
-    return this.coachesService.findAll();
+  @MessagePattern('coach.find.all')
+  findAll(@Query() findAllCoachesDto: FindAllCoachesDto) {
+    return this.coachesService.findAll(findAllCoachesDto);
   }
 
-  @MessagePattern('findOneCoach')
+  @MessagePattern('coach.find.one')
   findOne(@Payload() id: number) {
     return this.coachesService.findOne(id);
   }
 
-  @MessagePattern('updateCoach')
+  @MessagePattern('coach.update.one')
   update(@Payload() updateCoachDto: UpdateCoachDto) {
     return this.coachesService.update(updateCoachDto);
   }
 
-  @MessagePattern('removeCoach')
+  @MessagePattern('coach.remove.one')
   remove(@Payload() id: number) {
     return this.coachesService.remove(id);
   }
--- chess-auth-ms/src/modules/coaches/coaches.service.ts
+++ chess-auth-ms/src/modules/coaches/coaches.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Like, Repository } from 'typeorm';
 
 import { Coach } from './entities/coach.entity';
+
 import { CreateCoachDto, UpdateCoachDto } from './dto/create-coach.dto';
+import { FindAllCoachesDto } from './dto/find-all-coaches.dto';
+import { IMessage } from 'src/interfaces';
 
 @Injectable()
 export class CoachesService {
@@ -12,40 +16,135 @@ export class CoachesService {
     private readonly coachRepository: Repository<Coach>,
   ) {}
 
-  async create(createCoachDto: CreateCoachDto) {
-    const coach = this.coachRepository.create(createCoachDto);
-    return await this.coachRepository.save(coach);
+  async create(dto: CreateCoachDto): Promise<IMessage> {
+    const { name } = dto;
+
+    try {
+      const duplicateCoachName = await this.coachRepository.findOneBy({ name });
+      if (duplicateCoachName) {
+        throw new BadRequestException(
+          `Already exists a coach with name: ${name}`,
+        );
+      }
+
+      await this.coachRepository.save({ ...dto });
+
+      return { msg: `New coach with name: ${name} created successfully` };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 
-  async findAll() {
-    return await this.coachRepository.find({
-      order: { createdAt: 'DESC' },
-    });
+  async findAll(dto: FindAllCoachesDto) {
+    const { limit = 12, page = 1, name = null, isActive = null } = dto;
+    const offset = (page - 1) * limit;
+
+    const findOptions: FindManyOptions<Coach> = {
+      take: limit,
+      skip: offset,
+      order: {
+        name: 'ASC',
+      },
+    };
+
+    const whereConditions: any = {
+      // roles: Like(`%${SecurityRoles.TEACHER}%`),
+    };
+
+    if (name) {
+      whereConditions.name = Like(`%${name}%`);
+    }
+    if (isActive) {
+      const activeValue = isActive === 'YES';
+      whereConditions.isActive = activeValue;
+    }
+
+    if (Object.keys(whereConditions).length > 0) {
+      findOptions.where = whereConditions;
+    }
+
+    try {
+      const [coaches, total] =
+        await this.coachRepository.findAndCount(findOptions);
+
+      return {
+        total,
+        page,
+        coaches,
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 
   async findOne(id: number) {
-    const coach = await this.coachRepository.findOne({ where: { id } });
+    try {
+      const coach = await this.coachRepository.findOne({
+        where: { id },
+      });
+      if (!coach) {
+        throw new RpcException({
+          status: 401,
+          message: `Coach not found with ID: ${id}`,
+        });
+      }
 
-    if (!coach) throw new NotFoundException(`Coach #${id} not found`);
-
-    return coach;
+      return coach;
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 
-  async update(updateCoachDto: UpdateCoachDto) {
-    const { id, ...changes } = updateCoachDto;
+  async update(dto: UpdateCoachDto): Promise<IMessage> {
+    const { id, name = null, photoUrl = null, cvUrl = null } = dto;
 
-    const coach = await this.findOne(id); // ensures exists
+    try {
+      const coach = await this.findOne(id);
 
-    Object.assign(coach, changes);
+      // avoid coach name duplicity
+      if (name) {
+        const existsCoachByName = await this.coachRepository.findOneBy({
+          name,
+        });
+        if (existsCoachByName && existsCoachByName.id !== id) {
+          throw new BadRequestException(
+            `Already exists a coach with name: ${name}`,
+          );
+        }
+      }
 
-    return await this.coachRepository.save(coach);
+      // TODO: if photoUrl or cvUrl is needed to remove old files from fs
+
+      const updatedCoach = await this.coachRepository.preload({
+        ...coach,
+        ...dto,
+      });
+
+      await this.coachRepository.save(updatedCoach);
+
+      return { msg: `Coach with name: ${name} updated successfully.` };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<IMessage> {
     const coach = await this.findOne(id);
 
-    await this.coachRepository.remove(coach);
+    await this.coachRepository.update({ id }, { isActive: false });
 
-    return { message: `Coach #${id} removed` };
+    return { msg: `Coach with name: ${coach.name} removed successfully.` };
   }
 }
