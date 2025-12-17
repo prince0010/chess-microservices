ca175b20d14d8c9454d3bd2501c6a86b1cec26f8
onier9702 <onier0217@gmail.com>
2025-12-17 09:45:18 -0600

    [update] OrderApp controller developed and added in gateway to allow in app purchase verification from mobile app

--- chess-gateway/src/enum/order.enum.ts
+++ chess-gateway/src/enum/order.enum.ts
@@ -4,3 +4,8 @@ export enum OrderStatus {
   PAID = 'PAID',
   CANCELLED = 'CANCELLED',
 }
+
+export enum StorePlatform {
+  GOOGLE_PLAY_STORE = 'google_play',
+  APPLE_APP_STORE = 'app_store',
+}
--- chess-gateway/src/modules/payment/payment.module.ts
+++ chess-gateway/src/modules/payment/payment.module.ts
@@ -5,12 +5,14 @@ import { PaymentController } from './payment.controller';
 import { ItemController } from './item.controller';
 import { OrderBookLessonController } from './order-book-lesson.controller';
 import { NotificationPurchaseController } from './notification-purchase.controller';
+import { OrderAppController } from './order-app.controller';
 
 @Module({
   controllers: [
     PaymentController,
     ItemController,
     OrderBookLessonController,
+    OrderAppController,
     NotificationPurchaseController,
   ],
   providers: [],
